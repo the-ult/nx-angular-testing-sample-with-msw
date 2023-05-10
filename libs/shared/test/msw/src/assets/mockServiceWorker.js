d562c66e21c64d9316953b3cbf2c93d7a2511af2
The Ult <4863062+the-ult@users.noreply.github.com>
2023-05-10 15:41:47 +0200

    build(deps): update dependencies

--- libs/shared/test/msw/src/assets/mockServiceWorker.js
+++ libs/shared/test/msw/src/assets/mockServiceWorker.js
@@ -2,7 +2,7 @@
 /* tslint:disable */
 
 /**
- * Mock Service Worker (0.0.0-fetch.rc-1).
+ * Mock Service Worker (0.0.0-fetch.rc-2).
  * @see https://github.com/mswjs/msw
  * - Please do NOT modify this file.
  * - Please do NOT serve this file on production.
